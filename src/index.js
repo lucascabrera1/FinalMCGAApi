0a22b99eaad85cb28de6c247d51a081549638f7c
lucas_cabrera1 <lucas_cabrera1@hotmail.com>
2023-06-16 19:56:59 -0300

    redefinimos index para probar deploy en vercel

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
+import express from "express";
+import { Router } from 'express';
+import { ping } from "./Controllers/home";
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-import express from 'express';
-import router from './Routes/index.js'
-import {connect} from './connection.js'
-import cors from 'cors'
-import dotenv from 'dotenv'
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-dotenv.config({path: './.env'})
+const router = Router();
+router.route('/').get(ping);
+app.use(router);
 
-const app = express()
-const port = process.env.PORT
+dotenv.config({path:"./.env"});
 
-//uses
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
-app.set('appName', 'Recuperatorio MCGA 2021 back-end')
+let puerto = process.env.PORT;
 
-app.use((req, res, next) => {
-    res.send("paso por la funcion app.use")
-    console.log("paso por la funcion app.use")
-    next()
+app.listen(puerto, function() {
+        console.log("api de usuarios iniciada en puerto "+puerto);
 })
 
-app.listen(port, () =>{
-    console.log(`aplicacion ${app.get('appName')} corriendo en localhost puerto ${port}`)
-})
-
-//connect()
-
-export default app
+export default app;
